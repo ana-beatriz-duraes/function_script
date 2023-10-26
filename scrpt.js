1a6db9cd379365c8f16f81943b1576dc0348f94f
Ana Beatriz <anabeatrizduraes326@gmail.com>
2023-10-25 23:20:56 -0300

    first commit

---
+++ scrpt.js
@@ -0,0 +1,23 @@
+var valorCompra = prompt ("Insira o valor da mercadoria: ");
+var icms = (1-0.12);
+var valortotal = (valorCompra,icms)=>{ return valorCompra / icms}; 
+var valor_icms = (valortotal) => valortotal - valorCompra;
+
+console.log(valortotal(valorCompra,icms));
+console.log(valor_icms(valortotal(valorCompra,icms)));
+prompt(" Valor do icms é:"+(valor_icms(valortotal(valorCompra,icms)))+"valor total "+valortotal(valorCompra,icms));
+
+
+var n1 = prompt(" Insira o numero natural de maior valor que deseja saber se o numero de menor valor é seu multiplo: ");
+var n2 = prompt(" Insira o numero natural de menor valor que iremos calcular se ele é multiplo do primeiro numero inserido: ");
+
+function resultadoMultiplo (){
+    if(n1%n2 ==0){
+        prompt( n1+" multiplo "+n2);
+    } else{
+        prompt( n1+" não é multiplo "+n2);
+    }
+
+
+}
+console.log(resultadoMultiplo(n1,n2));
